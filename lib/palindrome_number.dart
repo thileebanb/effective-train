2834fb3074b7a2dceeab3c755e8c7d7c6c726788
Thileeban Balasundaram <thileeban.b@marketsimplified.com>
2025-04-27 20:14:11 +0530

    feat: add palindrome

--- lib/palindrome_number.dart
+++ lib/palindrome_number.dart
@@ -1,7 +1,17 @@
 bool isPalindrome(int x) {
-  return false;
+  if (x < 0) return false;
+
+  int reversed = 0;
+  int original = x;
+
+  while (x != 0) {
+    reversed = (x % 10) + reversed * 10;
+    x = x ~/ 10;
+  }
+
+  return (original == reversed);
 }
 
 void main(List<String> args) {
-  isPalindrome(121);
+  print(isPalindrome(121));
 }
